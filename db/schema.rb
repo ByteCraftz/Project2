d18dcf7a0494a3a9e4b97a0425700f59402db6a5
arifinsukses5yeah@gmail.com <arifinsukses5yeah@gmail.com>
2024-01-25 11:06:12 +0700

    yey gambarnya muncul

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_013656) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_033947) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_013656) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
